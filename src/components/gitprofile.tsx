dc2db8cca45f89742e8be9bf9ca1ca144f7259e4
Nitya Phani Santosh Oruganty <159747243+santhoshraghu26@users.noreply.github.com>
2025-05-23 21:13:37 -0400

    Update gitprofile.tsx

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -31,81 +31,46 @@ import BlogCard from './blog-card';
 import Footer from './footer';
 import PublicationCard from './publication-card';
 
-/**
- * Renders the GitProfile component.
- *
- * @param {Object} config - the configuration object
- * @return {JSX.Element} the rendered GitProfile component
- */
 const GitProfile = ({ config }: { config: Config }) => {
-  const [sanitizedConfig] = useState<SanitizedConfig | Record<string, never>>(
-    getSanitizedConfig(config),
-  );
+  const [sanitizedConfig] = useState<SanitizedConfig | Record<string, never>>(getSanitizedConfig(config));
   const [theme, setTheme] = useState<string>(DEFAULT_THEMES[0]);
   const [error, setError] = useState<CustomError | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [profile, setProfile] = useState<Profile | null>(null);
   const [githubProjects, setGithubProjects] = useState<GithubProject[]>([]);
 
-  const getGithubProjects = useCallback(
-    async (publicRepoCount: number): Promise<GithubProject[]> => {
-      if (sanitizedConfig.projects.github.mode === 'automatic') {
-        if (publicRepoCount === 0) {
-          return [];
-        }
-
-        const excludeRepo =
-          sanitizedConfig.projects.github.automatic.exclude.projects
-            .map((project) => `+-repo:${project}`)
-            .join('');
-
-        const query = `user:${sanitizedConfig.github.username}+fork:${!sanitizedConfig.projects.github.automatic.exclude.forks}${excludeRepo}`;
-        const url = `https://api.github.com/search/repositories?q=${query}&sort=${sanitizedConfig.projects.github.automatic.sortBy}&per_page=${sanitizedConfig.projects.github.automatic.limit}&type=Repositories`;
-
-        const repoResponse = await axios.get(url, {
-          headers: { 'Content-Type': 'application/vnd.github.v3+json' },
-        });
-        const repoData = repoResponse.data;
-
-        return repoData.items;
-      } else {
-        if (sanitizedConfig.projects.github.manual.projects.length === 0) {
-          return [];
-        }
-        const repos = sanitizedConfig.projects.github.manual.projects
-          .map((project) => `+repo:${project}`)
-          .join('');
-
-        const url = `https://api.github.com/search/repositories?q=${repos}+fork:true&type=Repositories`;
-
-        const repoResponse = await axios.get(url, {
-          headers: { 'Content-Type': 'application/vnd.github.v3+json' },
-        });
-        const repoData = repoResponse.data;
-
-        return repoData.items;
-      }
-    },
-    [
-      sanitizedConfig.github.username,
-      sanitizedConfig.projects.github.mode,
-      sanitizedConfig.projects.github.manual.projects,
-      sanitizedConfig.projects.github.automatic.sortBy,
-      sanitizedConfig.projects.github.automatic.limit,
-      sanitizedConfig.projects.github.automatic.exclude.forks,
-      sanitizedConfig.projects.github.automatic.exclude.projects,
-    ],
-  );
+  const getGithubProjects = useCallback(async (publicRepoCount: number): Promise<GithubProject[]> => {
+    if (sanitizedConfig.projects.github.mode === 'automatic') {
+      if (publicRepoCount === 0) return [];
+      const excludeRepo = sanitizedConfig.projects.github.automatic.exclude.projects
+        .map((project) => `+-repo:${project}`)
+        .join('');
+      const query = `user:${sanitizedConfig.github.username}+fork:${!sanitizedConfig.projects.github.automatic.exclude.forks}${excludeRepo}`;
+      const url = `https://api.github.com/search/repositories?q=${query}&sort=${sanitizedConfig.projects.github.automatic.sortBy}&per_page=${sanitizedConfig.projects.github.automatic.limit}&type=Repositories`;
+      const repoResponse = await axios.get(url, { headers: { 'Content-Type': 'application/vnd.github.v3+json' } });
+      return repoResponse.data.items;
+    } else {
+      if (sanitizedConfig.projects.github.manual.projects.length === 0) return [];
+      const repos = sanitizedConfig.projects.github.manual.projects.map((project) => `+repo:${project}`).join('');
+      const url = `https://api.github.com/search/repositories?q=${repos}+fork:true&type=Repositories`;
+      const repoResponse = await axios.get(url, { headers: { 'Content-Type': 'application/vnd.github.v3+json' } });
+      return repoResponse.data.items;
+    }
+  }, [
+    sanitizedConfig.github.username,
+    sanitizedConfig.projects.github.mode,
+    sanitizedConfig.projects.github.manual.projects,
+    sanitizedConfig.projects.github.automatic.sortBy,
+    sanitizedConfig.projects.github.automatic.limit,
+    sanitizedConfig.projects.github.automatic.exclude.forks,
+    sanitizedConfig.projects.github.automatic.exclude.projects,
+  ]);
 
   const loadData = useCallback(async () => {
     try {
       setLoading(true);
-
-      const response = await axios.get(
-        `https://api.github.com/users/${sanitizedConfig.github.username}`,
-      );
+      const response = await axios.get(`https://api.github.com/users/${sanitizedConfig.github.username}`);
       const data = response.data;
-
       setProfile({
         avatar: data.avatar_url,
         name: data.name || ' ',
@@ -113,22 +78,15 @@ const GitProfile = ({ config }: { config: Config }) => {
         location: data.location || '',
         company: data.company || '',
       });
-
-      if (!sanitizedConfig.projects.github.display) {
-        return;
+      if (sanitizedConfig.projects.github.display) {
+        setGithubProjects(await getGithubProjects(data.public_repos));
       }
-
-      setGithubProjects(await getGithubProjects(data.public_repos));
     } catch (error) {
       handleError(error as AxiosError | Error);
     } finally {
       setLoading(false);
     }
-  }, [
-    sanitizedConfig.github.username,
-    sanitizedConfig.projects.github.display,
-    getGithubProjects,
-  ]);
+  }, [sanitizedConfig.github.username, sanitizedConfig.projects.github.display, getGithubProjects]);
 
   useEffect(() => {
     if (Object.keys(sanitizedConfig).length === 0) {
@@ -147,15 +105,9 @@ const GitProfile = ({ config }: { config: Config }) => {
 
   const handleError = (error: AxiosError | Error): void => {
     console.error('Error:', error);
-
     if (error instanceof AxiosError) {
       try {
-        const reset = formatDistance(
-          new Date(error.response?.headers?.['x-ratelimit-reset'] * 1000),
-          new Date(),
-          { addSuffix: true },
-        );
-
+        const reset = formatDistance(new Date(error.response?.headers?.['x-ratelimit-reset'] * 1000), new Date(), { addSuffix: true });
         if (typeof error.response?.status === 'number') {
           switch (error.response.status) {
             case 403:
@@ -171,7 +123,7 @@ const GitProfile = ({ config }: { config: Config }) => {
         } else {
           setError(GENERIC_ERROR);
         }
-      } catch (innerError) {
+      } catch {
         setError(GENERIC_ERROR);
       }
     } else {
@@ -183,69 +135,29 @@ const GitProfile = ({ config }: { config: Config }) => {
     <HelmetProvider>
       <div className="fade-in h-screen">
         {error ? (
-          <ErrorPage
-            status={error.status}
-            title={error.title}
-            subTitle={error.subTitle}
-          />
+          <ErrorPage status={error.status} title={error.title} subTitle={error.subTitle} />
         ) : (
           <>
-            <HeadTagEditor
-              googleAnalyticsId={sanitizedConfig.googleAnalytics.id}
-              appliedTheme={theme}
-            />
+            <HeadTagEditor googleAnalyticsId={sanitizedConfig.googleAnalytics.id} appliedTheme={theme} />
             <div className={`p-4 lg:p-10 min-h-full ${BG_COLOR}`}>
               <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 rounded-box">
                 <div className="col-span-1">
                   <div className="grid grid-cols-1 gap-6">
                     {!sanitizedConfig.themeConfig.disableSwitch && (
-                      <ThemeChanger
-                        theme={theme}
-                        setTheme={setTheme}
+                      <ThemeChanger theme={theme} setTheme={setTheme} loading={loading} themeConfig={sanitizedConfig.themeConfig} />
+                    )}
+                    <AvatarCard profile={profile} loading={loading} avatarRing={sanitizedConfig.themeConfig.displayAvatarRing} resumeFileUrl={sanitizedConfig.resume.fileUrl} />
+                    <DetailsCard profile={profile} loading={loading} github={sanitizedConfig.github} social={sanitizedConfig.social} />
+                    {sanitizedConfig.skills.length !== 0 && <SkillCard loading={loading} skills={sanitizedConfig.skills} />}
+                    {sanitizedConfig.educations.length !== 0 && <EducationCard loading={loading} educations={sanitizedConfig.educations} />}
+                    {sanitizedConfig.experiences.length !== 0 && <ExperienceCard loading={loading} experiences={sanitizedConfig.experiences} />}
+                    {sanitizedConfig.awards?.length !== 0 && (
+                      <CertificationCard
                         loading={loading}
-                        themeConfig={sanitizedConfig.themeConfig}
+                        certifications={sanitizedConfig.awards}
+                        title="Awards"
                       />
                     )}
-                    <AvatarCard
-                      profile={profile}
-                      loading={loading}
-                      avatarRing={sanitizedConfig.themeConfig.displayAvatarRing}
-                      resumeFileUrl={sanitizedConfig.resume.fileUrl}
-                    />
-                    <DetailsCard
-                      profile={profile}
-                      loading={loading}
-                      github={sanitizedConfig.github}
-                      social={sanitizedConfig.social}
-                    />
-                    {sanitizedConfig.skills.length !== 0 && (
-                      <SkillCard
-                        loading={loading}
-                        skills={sanitizedConfig.skills}
-                      />
-                    )}
-                    {sanitizedConfig.educations.length !== 0 && (
-                      <EducationCard
-                        loading={loading}
-                        educations={sanitizedConfig.educations}
-                      />
-                    )}
-                    {sanitizedConfig.experiences.length !== 0 && (
-                      <ExperienceCard
-                        loading={loading}
-                        experiences={sanitizedConfig.experiences}
-                      />
-                    )}
-                    {(sanitizedConfig.certifications.length !== 0 || sanitizedConfig.awards?.length !== 0) && (
-                    <CertificationCard
-                      loading={loading}
-                      certifications={[
-                        ...(sanitizedConfig.certifications || []),
-                        ...(sanitizedConfig.awards || []),
-                      ]}
-                      title="Awards"
-                    />
-                  )}
                   </div>
                 </div>
                 <div className="lg:col-span-2 col-span-1">
@@ -261,19 +173,13 @@ const GitProfile = ({ config }: { config: Config }) => {
                       />
                     )}
                     {sanitizedConfig.publications.length !== 0 && (
-                      <PublicationCard
-                        loading={loading}
-                        publications={sanitizedConfig.publications}
-                      />
+                      <PublicationCard loading={loading} publications={sanitizedConfig.publications} />
                     )}
-                    {sanitizedConfig.projects.external.projects.length !==
-                      0 && (
+                    {sanitizedConfig.projects.external.projects.length !== 0 && (
                       <ExternalProjectCard
                         loading={loading}
                         header={sanitizedConfig.projects.external.header}
-                        externalProjects={
-                          sanitizedConfig.projects.external.projects
-                        }
+                        externalProjects={sanitizedConfig.projects.external.projects}
                         googleAnalyticId={sanitizedConfig.googleAnalytics.id}
                       />
                     )}
@@ -289,9 +195,7 @@ const GitProfile = ({ config }: { config: Config }) => {
               </div>
             </div>
             {sanitizedConfig.footer && (
-              <footer
-                className={`p-4 footer ${BG_COLOR} text-base-content footer-center`}
-              >
+              <footer className={`p-4 footer ${BG_COLOR} text-base-content footer-center`}>
                 <div className="card compact bg-base-100 shadow">
                   <Footer content={sanitizedConfig.footer} loading={loading} />
                 </div>
