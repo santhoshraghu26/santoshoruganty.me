f8cb7df0c7b5e9650abaf695cc82374fb876d6bf
Nitya Phani Santosh Oruganty <159747243+santhoshraghu26@users.noreply.github.com>
2025-05-25 19:03:31 -0400

    Update index.tsx

--- src/components/avatar-card/index.tsx
+++ src/components/avatar-card/index.tsx
@@ -1,3 +1,103 @@
+// import { FALLBACK_IMAGE } from '../../constants';
+// import { Profile } from '../../interfaces/profile';
+// import { skeleton } from '../../utils';
+// import LazyImage from '../lazy-image';
+
+// interface AvatarCardProps {
+//   profile: Profile | null;
+//   loading: boolean;
+//   avatarRing: boolean;
+//   resumeFileUrl?: string;
+// }
+
+// /**
+//  * Renders an AvatarCard component.
+//  * @param profile - The profile object.
+//  * @param loading - A boolean indicating if the profile is loading.
+//  * @param avatarRing - A boolean indicating if the avatar should have a ring.
+//  * @param resumeFileUrl - The URL of the resume file.
+//  * @returns JSX element representing the AvatarCard.
+//  */
+// const AvatarCard: React.FC<AvatarCardProps> = ({
+//   profile,
+//   loading,
+//   avatarRing,
+//   resumeFileUrl,
+// }): JSX.Element => {
+//   return (
+//     <div className="card shadow-lg compact bg-base-100">
+//       <div className="grid place-items-center py-8">
+//         {loading || !profile ? (
+//           <div className="avatar opacity-90">
+//             <div className="mb-8 rounded-full w-48 h-48">
+//               {skeleton({
+//                 widthCls: 'w-full',
+//                 heightCls: 'h-full',
+//                 shape: '',
+//               })}
+//             </div>
+//           </div>
+//         ) : (
+//           <div className="avatar opacity-90">
+//             <div
+//               className={`mb-8 rounded-full w-48 h-48 ${
+//                 avatarRing
+//                   ? 'ring ring-primary ring-offset-base-100 ring-offset-2'
+//                   : ''
+//               }`}
+//             >
+//               {
+//                 <LazyImage
+//                   src={profile.avatar ? profile.avatar : FALLBACK_IMAGE}
+//                   alt={profile.name}
+//                   placeholder={skeleton({
+//                     widthCls: 'w-full',
+//                     heightCls: 'h-full',
+//                     shape: '',
+//                   })}
+//                 />
+//               }
+//             </div>
+//           </div>
+//         )}
+//         <div className="text-center mx-auto px-8">
+//           <h5 className="font-bold text-2xl">
+//             {loading || !profile ? (
+//               skeleton({ widthCls: 'w-48', heightCls: 'h-8' })
+//             ) : (
+//               <span className="text-base-content opacity-70">
+//                 {profile.name}
+//               </span>
+//             )}
+//           </h5>
+//           <div className="mt-3 text-base-content text-opacity-60 font-mono">
+//             {loading || !profile
+//               ? skeleton({ widthCls: 'w-48', heightCls: 'h-5' })
+//               : profile.bio}
+//           </div>
+//         </div>
+//         {resumeFileUrl &&
+//           (loading ? (
+//             <div className="mt-6">
+//               {skeleton({ widthCls: 'w-40', heightCls: 'h-8' })}
+//             </div>
+//           ) : (
+//             <a
+//               href={resumeFileUrl}
+//               target="_blank"
+//               className="btn btn-outline btn-sm text-xs mt-6 opacity-50"
+//               download
+//               rel="noreferrer"
+//             >
+//               Download Resume
+//             </a>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AvatarCard;
 import { FALLBACK_IMAGE } from '../../constants';
 import { Profile } from '../../interfaces/profile';
 import { skeleton } from '../../utils';
@@ -12,11 +112,6 @@ interface AvatarCardProps {
 
 /**
  * Renders an AvatarCard component.
- * @param profile - The profile object.
- * @param loading - A boolean indicating if the profile is loading.
- * @param avatarRing - A boolean indicating if the avatar should have a ring.
- * @param resumeFileUrl - The URL of the resume file.
- * @returns JSX element representing the AvatarCard.
  */
 const AvatarCard: React.FC<AvatarCardProps> = ({
   profile,
@@ -26,7 +121,8 @@ const AvatarCard: React.FC<AvatarCardProps> = ({
 }): JSX.Element => {
   return (
     <div className="card shadow-lg compact bg-base-100">
-      <div className="grid place-items-center py-8">
+      <div className="grid place-items-center py-8 px-4">
+        {/* Avatar */}
         {loading || !profile ? (
           <div className="avatar opacity-90">
             <div className="mb-8 rounded-full w-48 h-48">
@@ -46,36 +142,38 @@ const AvatarCard: React.FC<AvatarCardProps> = ({
                   : ''
               }`}
             >
-              {
-                <LazyImage
-                  src={profile.avatar ? profile.avatar : FALLBACK_IMAGE}
-                  alt={profile.name}
-                  placeholder={skeleton({
-                    widthCls: 'w-full',
-                    heightCls: 'h-full',
-                    shape: '',
-                  })}
-                />
-              }
+              <LazyImage
+                src={profile.avatar ? profile.avatar : FALLBACK_IMAGE}
+                alt={profile.name}
+                placeholder={skeleton({
+                  widthCls: 'w-full',
+                  heightCls: 'h-full',
+                  shape: '',
+                })}
+              />
             </div>
           </div>
         )}
+
+        {/* Name */}
         <div className="text-center mx-auto px-8">
-          <h5 className="font-bold text-2xl">
+          <h5 className="font-bold text-2xl text-base-content">
             {loading || !profile ? (
               skeleton({ widthCls: 'w-48', heightCls: 'h-8' })
             ) : (
-              <span className="text-base-content opacity-70">
-                {profile.name}
-              </span>
+              <span>{profile.name}</span>
             )}
           </h5>
+
+          {/* Bio */}
           <div className="mt-3 text-base-content text-opacity-60 font-mono">
             {loading || !profile
               ? skeleton({ widthCls: 'w-48', heightCls: 'h-5' })
               : profile.bio}
           </div>
         </div>
+
+        {/* Resume Button */}
         {resumeFileUrl &&
           (loading ? (
             <div className="mt-6">
@@ -85,7 +183,7 @@ const AvatarCard: React.FC<AvatarCardProps> = ({
             <a
               href={resumeFileUrl}
               target="_blank"
-              className="btn btn-outline btn-sm text-xs mt-6 opacity-50"
+              className="mt-6 btn btn-primary text-white px-6 py-2 text-base font-semibold rounded-lg shadow-md hover:scale-105 transition-transform duration-200"
               download
               rel="noreferrer"
             >
