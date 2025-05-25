f7214cae3a6c215e1a80041fe03041820968e335
Nitya Phani Santosh Oruganty <159747243+santhoshraghu26@users.noreply.github.com>
2025-05-25 12:40:55 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,8 @@
 
 const CONFIG = {
   github: {
-    username: 'santhoshraghu26', // Your GitHub org/user name. (This is the only required config)
+    username: 'santhoshraghu26',
+    displayName: 'santhoshraghu',// Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
